64584d8b807d2da8e09a5a0f8a94eb064004e7bc
George Beckert <geor>
2017-12-23 13:37:09 -0500

    moved the tile and Rect classes to another file

--- Esper.py
+++ Esper.py
@@ -3,6 +3,7 @@ from random import randint
 import colors
 import math
 import textwrap
+import Map
 #important  variables..
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
@@ -20,6 +21,7 @@ MAP_WIDTH = 80
 MAP_HEIGHT = 43
 
 ROOM_MAX_SIZE = 30 #defualt for tutorial was 10 and 6
+
 ROOM_MIN_SIZE = 2
 MAX_ROOMS = 80
 
@@ -51,38 +53,8 @@ color_light_floor = colors.lightfloor
 #ABILITIES                          #
 #####################################
 HEALABL = False
-
-class Tile:
-    #A tile of the map and its properties
-    def __init__(self, blocked, block_sight=None):
-        self.blocked = blocked
-
-        self.explored = False
-
-        #by default if a tile is blocked it also blocks sight
-        if block_sight is None:
-            block_sight = blocked
-        self.block_sight = block_sight
-
-class Rect:
-    #A rectangle on the map. used to characterize a aroom
-    def __init__(self, x, y, w, h):
-        self.x1 = x
-        self.y1 = y
-        self.x2 = x + w
-        self.y2 = y + h
-
-    def center(self):
-        center_x = (self.x1 + self.x2) //2
-        center_y = (self.y1 + self.y2) //2
-        return(center_x, center_y)
-
-    def intersect(self, other):
-        #returns true if this rectangle intersects with another
-        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-                self.y1 <= other.y2 and self.y2 >= other.y1)
-
-
+Tile = Map.Tile;
+Rect = Map.Rect;
 class GameObject:
     #A generic object, could be: the player, a monster, an item, the stairs
     #always represented by an ASCII character on screen.
